f798efd96d064dd5f2816b7c363cb089697e503f
ShivanshMishra18 <45760908+ShivanshMishra18@users.noreply.github.com>
2019-11-04 17:17:41 +0530

    Create max_cost_spanning_tree.cpp

---
+++ PostMidsemLab_5/max_cost_spanning_tree.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define LD long double
+
+int par[10000];
+int r[10000];
+
+int find(int a)
+{
+	if (a == par[a])
+		return a;
+
+	return par[a] = find(par[a]);
+}
+
+bool join(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+
+	if (a==b)	return false;
+
+	if (r[a] < r[b])
+		swap (a,b);
+
+	par[b] = a;
+	if (r[a] == r[b])
+		r[a]++;
+
+	return true;
+}
+
+int main()
+{
+	long long weight = 1;
+
+	int n,m;
+	cin >> n >> m;
+
+	vector <pair < long long, pair<int,int> > > v;
+
+	for (int i=0; i<n; i++)
+	{
+		par[i] = i;
+		r[i] = 1;
+	}
+
+	for (int i=0; i<m; i++)
+	{
+		long long k;
+		int a,b;
+		cin >> a >> b >> k;
+
+		v.push_back(make_pair(k, make_pair(a,b)));
+	}
+
+	sort(v.rbegin(), v.rend());
+
+	for (auto it: v)
+	{
+		long long k;
+		int a,b;
+
+		k = it.first;
+		a = it.second.first;
+		b = it.second.second;
+
+		if (join(a,b))
+			weight = (weight * k) % 1000000007;
+	}
+
+	cout << weight << "\n";
+	return 0;
+}
